f3d5a17e893ed301cf95558401c2f25369f89ae3
HU Yifan <yfan_hu@qq.com>
2019-10-10 23:39:11 +0800

    add static js add static in production env add bootstrap add collect_static

--- django_gitlab_web/settings.py
+++ django_gitlab_web/settings.py
@@ -113,6 +113,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static')
+
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'static'),
 ]
--- django_gitlab_web/urls.py
+++ django_gitlab_web/urls.py
@@ -14,39 +14,46 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,re_path
-from gitlab_web.views import functional_views,views,views_qqmusic_download
+from django.urls import path, re_path
+from gitlab_web.views import functional_views, views, views_qqmusic_download
+from django.contrib.staticfiles.views import serve
+
+
+def return_static(request, path, insecure=True, **kwargs):
+    return serve(request, path, insecure, **kwargs)
+
 
 urlpatterns = [
+    re_path(r'^static/(?P<path>.*)$', return_static, name='static'),
     # path('admin/', admin.site.urls),
-    path('download/',views.download),
-    path('hyf2019.com',views.index),
-    path('account_sharing',views.account_sharing_verify),
+    path('download/', views.download),
+    path('hyf2019.com', views.index),
+    path('account_sharing', views.account_sharing_verify),
 
     path('super', views.super_admin),
-    path('super/delete_admin/',views.super_delete_admin),
-    path('super/approve_apply/',views.super_approve_apply),
-    path('super/refuse_apply/',views.super_refuse_apply),
+    path('super/delete_admin/', views.super_delete_admin),
+    path('super/approve_apply/', views.super_approve_apply),
+    path('super/refuse_apply/', views.super_refuse_apply),
     path('super/approve_register/', views.super_approve_register),
     path('super/refuse_register/', views.super_refuse_register),
 
     re_path(r'^test/?$', views.test),
-    path('account_sharing/verify_code',views.account_sharing_verify),
-    path('account_sharing/admin',views.account_admin),
-    path('account_sharing/admin/login',views.account_admin_login),
-    path('account_sharing/admin/logout',views.account_admin_logout),
-    path('account_sharing/admin/change_password',views.account_admin_change_password),
-    path('account_sharing/admin/register',views.account_admin_register),
-    path('account_sharing/admin/update_account/',views.account_admin_update),
-    path('account_sharing/admin/add_account',views.account_admin_add),
-    path('account_sharing/admin/delete_account/',views.account_admin_delete),
-    path('account_sharing/admin/delete_history/',views.account_history_delete),
-    path('account_sharing/admin/delete_apply/',views.admin_delete_apply),
-    path('account_sharing/verify_admin_username/',functional_views.account_sharing_verify_admin_username),
+    path('account_sharing/verify_code', views.account_sharing_verify),
+    path('account_sharing/admin', views.account_admin),
+    path('account_sharing/admin/login', views.account_admin_login),
+    path('account_sharing/admin/logout', views.account_admin_logout),
+    path('account_sharing/admin/change_password', views.account_admin_change_password),
+    path('account_sharing/admin/register', views.account_admin_register),
+    path('account_sharing/admin/update_account/', views.account_admin_update),
+    path('account_sharing/admin/add_account', views.account_admin_add),
+    path('account_sharing/admin/delete_account/', views.account_admin_delete),
+    path('account_sharing/admin/delete_history/', views.account_history_delete),
+    path('account_sharing/admin/delete_apply/', views.admin_delete_apply),
+    path('account_sharing/verify_admin_username/', functional_views.account_sharing_verify_admin_username),
     path('account_sharing/verify_invitation_code/', functional_views.account_sharing_verify_invitation_code),
     # /account_sharing/add_log/2019-08-23%2015:55:08/4h
-    re_path(r'^account_sharing/add_log/(.+)/(\w+)/(\w+)$',views.add_log),
+    re_path(r'^account_sharing/add_log/(.+)/(\w+)/(\w+)$', views.add_log),
 
-    path('music_download',views_qqmusic_download.index),
-    path('music_download/qqmusic_download',views_qqmusic_download.download_music),
+    path('music_download', views_qqmusic_download.index),
+    path('music_download/qqmusic_download', views_qqmusic_download.download_music),
 ]
